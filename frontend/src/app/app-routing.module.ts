d0f2ede70f50ae064ba49e6a737f191459cdeb32
jeissonzh <jeissonzh@hotmail.com>
2021-12-16 21:47:58 -0500

    ruteo correcto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,15 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/login/login.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { NuevoInmuebleComponent } from './components/nuevo-inmueble/nuevo-inmueble.component';
 
 const routes: Routes = [
-  {path:"login", component: LoginComponent},
-  {path:"inicio", component: BusquedaComponent},
-  {path:"inicio", component: InicioComponent},
-  {path:"inicio", component: NavbarComponent},
-  {path:"inicio", component: NuevoInmuebleComponent}
+  {path:"", component: InicioComponent},
+  {path:"login", component: LoginComponent}
 ];
 
 @NgModule({
